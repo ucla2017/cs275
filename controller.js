3326eb283b5f2ddc8f890faa51430d13e013b80b
Jian Gong <git@jiangong.me>
2014-02-23 14:26:00 -0800

    add controller

---
+++ controller.js
@@ -0,0 +1,128 @@
+
+
+//global variable
+var numPillar = 10;
+var numVisiblePillar = 2;
+var safeDis = 10;	//size of no pillar area
+var pillars = [];	//the queue for pillars
+var visible = [16];	//the visible data for ML
+var gravity = -0.01;//(mm/ms^2)
+var vJump = 10;		//constant velocity on Y-axis of the bird  	(m/s)
+var vMove = -1;	//constant velocity on X-axis of pillars	(m/s)
+var score = 0;		//score of the game
+
+var Controller = { REVISION: '01' };
+
+//an object is a function
+function Bird(r, y, v, fovy)
+{
+	this.r = r;
+	this.y = y;
+	this.v = v;
+	this.fovy = fovy;//degree
+}
+var bird = new Bird(1, 0, 0, 60);
+
+function Pillar(x)
+{
+	//fixed width, height, disBetweenPillar
+	this.x = x;
+	this.y = (Math.random() - 0.5) * 20;
+	this.w = 3;
+	this.h = 4;
+}
+
+Controller.init = function() {
+	//initialization		
+	for(var i = 0, x = safeDis; i < numPillar; ++i) {
+		pillars.push(new Pillar(x));
+		x += pillars[i].w + 6;//disBetweenPillar
+	}
+	score = 0;
+}
+
+//call main when user start the game
+Controller.update = function (delta) {
+	//check visible
+	vision();
+	//update the bird
+	hasJumped = jump(visible[16]);
+	if (hasJumped) bird.v = vJump;
+	update(delta);
+	//collapse detection
+	return [collision(), hasJumped];
+}
+
+function vision()
+{
+	var first = 0, second = 1;
+	if (pillars[0].x + pillars[0].w <= 0) first = 1, second = 2;
+	//initial value
+	visible[0]  = visible[2]  = pillars[first].x;
+	visible[4]  = visible[6]  = pillars[first].x + pillars[first].w;
+	visible[1]  = visible[5]  = pillars[first].y + pillars[first].h/2 - bird.y;
+	visible[3]  = visible[7]  = pillars[first].y - pillars[first].h/2 - bird.y;
+	visible[8]  = visible[10] = pillars[second].x;
+	visible[12] = visible[14] = pillars[second].x + pillars[second].w;
+	visible[9]  = visible[13] = pillars[second].y + pillars[second].h/2 - bird.y;
+	visible[11] = visible[15] = pillars[second].y - pillars[second].h/2 - bird.y;
+	//visible check, x = -1 for invisible
+	var k = Math.tan(bird.fovy * Math.PI / 360);
+	for(var i = 14; i >= 0; i -= 2) {
+		//fovy check
+		if (visible[i] <= 0 || visible[i+1] > visible[i] * k || visible[i+1] < visible[i] * -k) {
+			visible[i] = -1;	//x = -1 for invisible
+			continue;
+		}
+		//block check
+		for(var j = 0; j < i - 2; j += 4) {
+			if (visible[j] > 0 && 
+				(visible[i+1] * visible[j] > visible[i] * visible[j+1] || visible[i+1] * visible[j] < visible[i] * visible[j+3])) {
+				visible[i] = -1;
+				break;
+			}
+		}
+	}
+}
+
+function update(delta)
+{
+	bird.y += delta * (bird.v + gravity * delta / 2);	//s = vt + att/2
+	bird.v += delta * gravity;
+	//update the pillars
+	for(var i = 0; i < numPillar; ++i) 
+		pillars[i].x += delta * vMove;
+	while(pillars.length > 0 && pillars[0].x + pillars[0].w <= -bird.r) {
+		pillars.shift();
+		++score;
+	}
+	if (pillars.length == 0) pillars.push(new Pillar(safeDis));
+	for(var i = pillars.length; i < numPillar; ++i)
+		pillars.push(new Pillar(pillars[i-1].x + 9));	
+}
+
+function collision()
+{
+	var dx, dy;
+	for(var i = 0; i < numPillar; ++i) {
+		if (pillars[i].x > bird.r) break;
+		if (bird.y <= pillars[i].y - pillars[i].h/2 || bird.y >= pillars[i].y + pillars[i].h/2) return true;
+		if (pillars[i].x <= 0 && 0 <= pillars[i].x + pillars[i].w && 
+			(bird.y <= pillars[i].y - pillars[i].h/2 + bird.r || bird.y >= pillars[i].y + pillars[i].h/2 - bird.r)) return true;
+		dx = (0 <= pillars[i].x ? pillars[i].x : pillars[i].x + pillars[i].w);
+		dy = bird.y - (bird.y <= pillars[i].y ? pillars[i].y - pillars[i].h/2 : pillars[i].y + pillars[i].h/2);
+		if (dx * dx + dy * dy <= bird.r * bird.r) return true;
+	}
+	return false;
+}
+
+//Hang's task
+function jump()
+{
+	return (Math.random < 0.5)	
+}
+
+//Jian's task
+function display()
+{
+}
---
+++ graphics.js
@@ -0,0 +1,254 @@
+
+var Graphics = ( function () {
+
+	var Graphics = { REVISION: '01' };
+
+	var camera, scene, renderer;
+	var cameraCube, sceneCube;
+
+	var mesh, lightMesh, geometry;
+	var spheres = [];
+
+	var morphs = [];
+	
+	var animRemainTime = 0;
+	var animBeginTime = 650;
+
+	var directionalLight, pointLight;
+
+
+	var cylinderHeight = 40;
+
+	Graphics.init = function (container) {
+			camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 1, 100000 );
+			camera.position.z = 50;
+
+			cameraCube = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 1, 100000 );
+
+			scene = new THREE.Scene();
+			sceneCube = new THREE.Scene();
+
+			var radiusTop = 1.5;
+			var segmentsRadius = 50;
+			var segmentsHeight = 50;
+			var radiusBottom = radiusTop;
+
+			var geometry = new THREE.CylinderGeometry( radiusTop, radiusBottom, cylinderHeight, segmentsRadius, segmentsHeight);
+
+			var path = "textures/cube/skybox/";
+			var format = '.jpg';
+			var urls = [
+				path + 'px' + format, path + 'nx' + format,
+				path + 'py' + format, path + 'ny' + format,
+				path + 'pz' + format, path + 'nz' + format
+			];
+
+			var textureCube = THREE.ImageUtils.loadTextureCube( urls, new THREE.CubeRefractionMapping() );
+			var material = new THREE.MeshBasicMaterial( { color: 0xffffff, envMap: textureCube, refractionRatio: 0.95 } );
+			var material2 = new THREE.MeshPhongMaterial( { ambient: 0x4EB81A, color: 0x4EB81A, specular: 0x4EB81A, shininess: 30, shading: THREE.FlatShading } );
+
+			for ( var i = 0; i < 20; i ++ ) {
+
+				var mesh = new THREE.Mesh( geometry, material2 );
+
+				mesh.position.x = Math.random() * 100 - 50;
+				mesh.position.y = Math.random() * 100 - 50;
+				mesh.position.z = Math.random() * 100 - 50;
+
+				mesh.scale.x = mesh.scale.y = mesh.scale.z = 1;
+
+				scene.add( mesh );
+
+				spheres.push( mesh );
+
+			}
+
+			// Skybox
+			initSkybox(textureCube);
+			function initSkybox(textureCube) {
+				var shader = THREE.ShaderLib[ "cube" ];
+				shader.uniforms[ "tCube" ].value = textureCube;
+
+				var material = new THREE.ShaderMaterial( {
+
+					fragmentShader: shader.fragmentShader,
+					vertexShader: shader.vertexShader,
+					uniforms: shader.uniforms,
+					depthWrite: false,
+					side: THREE.BackSide
+
+				} ),
+
+				mesh = new THREE.Mesh( new THREE.BoxGeometry( 100, 100, 100 ), material );
+				sceneCube.add( mesh );
+			}
+
+			// LIGHTS
+
+			hemiLight = new THREE.HemisphereLight( 0xffffff, 0xffffff, 0.6 );
+			hemiLight.color.setHSL( 1, 1, 1 );
+			hemiLight.groundColor.setHSL( 0.095, 1, 0.75 );
+			hemiLight.position.set( 0, 500, 0 );
+			scene.add( hemiLight );
+
+			// MORPHS
+			function morphColorsToFaceColors( geometry ) {
+
+				if ( geometry.morphColors && geometry.morphColors.length ) {
+
+					var colorMap = geometry.morphColors[ 0 ];
+
+					for ( var i = 0; i < colorMap.colors.length; i ++ ) {
+
+						geometry.faces[ i ].color = colorMap.colors[ i ];
+
+					}
+
+				}
+
+			}
+
+			var loader = new THREE.JSONLoader();
+
+			var startX = 0;
+
+			loader.load( "models/animated/parrot.js", function( geometry ) {
+
+				morphColorsToFaceColors( geometry );
+
+				geometry.computeMorphNormals();
+
+				var material = new THREE.MeshPhongMaterial( { color: 0xffffff, specular: 0xffffff, shininess: 20, morphTargets: true, morphNormals: true, vertexColors: THREE.FaceColors, shading: THREE.FlatShading } );
+				var meshAnim = new THREE.MorphAnimMesh( geometry, material );
+
+				meshAnim.duration = 1000;
+				meshAnim.time = animBeginTime;
+
+				var s = 0.1;
+				meshAnim.scale.set( s, s, s );
+				meshAnim.position.set(0, 0, 0);
+				//meshAnim.position.y = 0;
+				meshAnim.rotation.y = Math.PI / 2;
+
+				meshAnim.castShadow = true;
+				meshAnim.receiveShadow = true;
+
+				scene.add( meshAnim );
+				morphs.push( meshAnim );
+
+				
+			} );
+
+			//
+			renderer = new THREE.WebGLRenderer();
+			renderer.setSize( window.innerWidth, window.innerHeight );
+			renderer.autoClear = false;
+			container.appendChild( renderer.domElement );
+	};
+
+	Graphics.resizeWindow = function () {
+
+		windowHalfX = window.innerWidth / 2,
+		windowHalfY = window.innerHeight / 2,
+
+		camera.aspect = window.innerWidth / window.innerHeight;
+		camera.updateProjectionMatrix();
+
+		cameraCube.aspect = window.innerWidth / window.innerHeight;
+		cameraCube.updateProjectionMatrix();
+
+		renderer.setSize( window.innerWidth, window.innerHeight );
+
+	};
+
+	Graphics.update = function (mouseX, mouseY, delta, elapseTime, pillars, bird) {
+
+
+		for ( var i = 0, il = 10; i < il; i += 1 ) {
+
+			var sphere1 = spheres[ i*2 ];
+			var sphere2 = spheres[ i*2 + 1 ];
+
+			sphere1.position.x = pillars[i].x;
+			sphere1.position.y = pillars[i].y + pillars[i].h/2 + cylinderHeight / 2;
+			sphere1.position.z = 0;
+
+			sphere2.position.x = pillars[i].x;
+			sphere2.position.y = pillars[i].y - pillars[i].h/2 - cylinderHeight / 2;
+			sphere2.position.z = 0;
+		}
+
+		// animate bird
+		
+
+		if (animRemainTime > 0) {
+
+			animRemainTime -= delta;
+
+			for ( var i = 0; i < morphs.length; i ++ ) {
+
+				morph = morphs[ i ];
+				morph.updateAnimation( 1000 * delta );
+				morph.position.y = bird.y;
+			}
+		}
+
+		// update camera
+		camera.position.x += ( mouseX / 40.0 - camera.position.x ) * .05;
+		camera.position.y += ( - mouseY / 40.0 - camera.position.y ) * .05;
+
+		camera.lookAt( scene.position );
+		cameraCube.rotation.copy( camera.rotation );
+
+		// update status and log text
+		str = 'mouseX = ' + mouseX + ', mouseY = ' + mouseY + ', ';
+		str += 'delta = ' + delta.toFixed(2) + ', ';
+		str += 'ElapsedTime = ' + elapseTime.toFixed(2) + ', ';
+		str += 'animRemainTime = ' + animRemainTime.toFixed(2) + ', ';
+		str += 'bird.x = ' + bird.x + ', bird.y = ' + bird.y.toFixed(2) + ', ';
+		str += 'pillars[0].x = ' + pillars[0].x.toFixed(2) + '<br>';
+		Graphics.setStatus(str);
+		if (Math.round(elapseTime*10) % 10 == 0) {
+			Graphics.setLog('...');
+		}
+
+		renderer.render( sceneCube, cameraCube );
+		renderer.render( scene, camera );
+
+	};
+
+	Graphics.flapWings = function () {
+		if (animRemainTime <= 0) {
+
+			animRemainTime = 1;
+
+			for ( var i = 0; i < morphs.length; i ++ ) {
+
+				morph = morphs[ i ];
+				morph.time = animBeginTime;
+
+			}
+		}
+	};
+
+	Graphics.setBirdPosition = function (x, y, z) {
+		for ( var i = 0; i < morphs.length; i ++ ) {
+
+			morph = morphs[ i ];
+			morph.position.set(x, y, z);
+
+		}
+	};
+
+	Graphics.setStatus = function (text) {
+		document.getElementById('status').innerHTML = text;
+	};
+
+	Graphics.setLog = function (text) {
+		document.getElementById('log').innerHTML = text;
+	};
+
+	return Graphics;
+
+}() );
+
