41c8d5e3f8ffa5a2f4d3b9be4412321f75c0197d
Lunbo <alexanderxu818@gmail.com>
2014-02-25 15:44:15 -0800

    add data range on learning.js

--- learning.js
+++ learning.js
@@ -4,5 +4,7 @@ Learning.jump = function jump()
 {	
 	//return false;
 	//you can directly read visible[0] ~ visible[15] here
+	//x: -1 for invisible, range from 0~70
+	//y: -50 ~ 50 (it depends on fovy actually)
 	return (Math.random() < 0.05);
 }
